df8359da68873c455e99210aed9c85fb7d2414ed
Jaime Lapiz <jaime.lapiz@usjr.edu.ph>
2021-10-30 23:17:26 +0800

    updated book-flight

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
+
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
+import { MatStepperModule } from '@angular/material/stepper';
+
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+
+
 import { NoopAnimationsModule, BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
@@ -28,6 +57,29 @@ import { LogoutComponent } from './logout/logout.component';
     AppRoutingModule,
     NoopAnimationsModule,
     BrowserAnimationsModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatTabsModule,
+    MatSidenavModule,
+    MatListModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatProgressSpinnerModule,
+    MatDialogModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatRadioModule,
+    MatCheckboxModule,
+    MatStepperModule,
+    MatProgressBarModule,
+    FormsModule,
+    ReactiveFormsModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore())
--- src/app/book-flight/book-flight.component.ts
+++ src/app/book-flight/book-flight.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import {FormGroup, Validators, FormBuilder} from '@angular/forms';
+
+interface Destination {
+  value: string;
+  viewValue: string;
+}
+
+interface Airline {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-book-flight',
@@ -7,9 +18,89 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookFlightComponent implements OnInit {
 
-  constructor() { }
+  @Output() submitted = new EventEmitter<boolean>();
+  
+  selectedDes = "";
+  selectedOri = "";
+  selectedAir = "";
+  selectedTrip ="";
+
+  bookForm = this.fb.group ({
+    airLine: ['', Validators.required],
+    orig: ['', Validators.required],
+    dest: ['', Validators.required],
+    trip: ['', Validators.required],
+    depDate: ['', Validators.required],
+    retDate: [''],
+  });
+
+  constructor(private fb: FormBuilder) { } 
+
+  destinations: Destination [] = [
+    {value: 'cdo-0', viewValue: 'Cagayan De Oro'},
+    {value: 'cam-1', viewValue: 'Camiguin'},
+    {value: 'ceb-2', viewValue: 'Cebu'},
+    {value: 'dav-3', viewValue: 'Davao'},
+    {value: 'gensan-4', viewValue: 'General Santos'},
+    {value: 'kal-5', viewValue: 'Kalibo'},
+    {value: 'metman-6', viewValue: 'Metro Manila'},
+    {value: 'oza-7', viewValue: 'Ozamiz'},
+    {value: 'pagad-8', viewValue: 'Pagadian'},
+    {value: 'puerto-9', viewValue: 'Puerto Princesa'},
+    {value: 'siar-10', viewValue: 'Siargao'},    
+  ];
+  
+  airlines: Airline [] = [
+    {value: 'swift-0', viewValue: 'Air Swift'},
+    {value: 'go-1', viewValue: 'Cebgo'},
+    {value: 'pacific-2', viewValue: 'Cebu Pacific'},
+    {value: 'asia-3', viewValue: 'Philippines AirAsia'},
+    {value: 'airlines-4', viewValue: 'Philippine Airlines'},   
+    {value: 'jet-5', viewValue: 'SkyJet Airlines Philippines'},
+    {value: 'pasada-6', viewValue: 'Sky Pasada'},
+  ];
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    const flightRec = {
+      airLine: this.bf.airLine.value,
+      orig: this.bf.orig.value,
+      dest: this.bf.dest.value,
+      trip: this.bf.trip.value,
+      depDate: this.bf.depDate.value,
+      retDate: this.bf.retDate.value,
+    };
+
+    
+  }
+
+  get airLine() {
+    return this.bookForm.controls['airLine'];
+  }
+
+  get orig() {
+    return this.bookForm.controls['orig'];
+  }
+
+  get dest() {
+    return this.bookForm.controls['dest'];
+  }
+
+  get trip() {
+    return this.bookForm.controls['trip'];
+  }
+
+  get depDate() {
+    return this.bookForm.controls['depDate'];
+  }
+
+  get retDate() {
+    return this.bookForm.controls['retDate'];
+  }
+
+  get bf() {
+    return this.bookForm.controls;
+  }
 }
