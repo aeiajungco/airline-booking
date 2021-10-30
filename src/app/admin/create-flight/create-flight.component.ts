1982d2139444dcc589f68b236f9f97c2cfbb2227
Angelika Jungco <angelika.jungco@usjr.edu.ph>
2021-10-31 00:04:38 +0800

    create-flight

--- src/app/admin/create-flight/create-flight.component.ts
+++ src/app/admin/create-flight/create-flight.component.ts
@@ -1,3 +1,4 @@
+import { FormBuilder } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateFlightComponent implements OnInit {
 
-  constructor() { }
+  form = this.fb.group ({
+    
+  }
+  )
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,3 +9,4 @@ export const environment = {
   },
   production: true
 };
+
