4dc9f447b49d6aac0b909cab52079ca646d271ab
Angelika Jungco <angelika.jungco@usjr.edu.ph>
2021-11-03 21:56:47 +0800

    updated routing

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
     component: AdminHomeComponent,
     children: [
       {
-        path: 'admin',
+        path: '',
         component: AdminHomeComponent,
       },
       {
