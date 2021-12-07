238e8c0bd54bbb6221657be61d2b5eb20180bd83
Angelika Jungco <angelika.jungco@usjr.edu.ph>
2021-12-08 01:04:49 +0800

    updated UI

--- src/app/admin/view-users/view-users.component.ts
+++ src/app/admin/view-users/view-users.component.ts
@@ -1,8 +1,7 @@
-import { BookingListComponent } from './../../user/booking-list/booking-list.component';
 import { LoginVarService } from './../../services/login-var.service';
-import { FlightsService } from './../../services/flights.service';
 import { UserService } from 'src/app/services/user.service';
 import { Component, OnInit } from '@angular/core';
+import * as $ from "jquery";
 
 @Component({
   selector: 'app-view-users',
@@ -17,6 +16,7 @@ export class ViewUsersComponent implements OnInit {
   flightList$: any = [];
   un = ''
   empty: any;
+  userWithBookings: any = [];
 
   constructor(
     private users: UserService,
@@ -36,6 +36,11 @@ export class ViewUsersComponent implements OnInit {
   showBookings(username: any) {
     this.un = ''
     this.show = !this.show;
+
+    // $('button[id^="view-btn"]').eq(index).text(function(i, text) {
+    //   return text === "Hide Bookings" ? "View Bookings" : "Hide Bookings";
+    // });
+
     for (let x of this.bookingList$) {
       if (username == x.username) {
         this.un = x.username
@@ -43,5 +48,30 @@ export class ViewUsersComponent implements OnInit {
         break;
       }
     }
+
+    if (this.un == '') {
+      console.log("EMPTY");
+      this.empty = true;
+      for (let i = 0; i <= this.userWithBookings.length; i++) {
+        if(this.userWithBookings.length == 0) {
+          this.userWithBookings.push(username);
+          console.log("PASSED BY");
+        }
+        else if (username != this.userWithBookings[i] && i+1 == this.userWithBookings.length) {
+          this.userWithBookings.push(username);
+          console.log("PASSED BY");
+        }
+        if (username == this.userWithBookings[i]) {
+          this.un = this.userWithBookings[i];
+          console.log("PASSED BY");
+        }
+      }
+   }
+
+   console.log(this.userWithBookings);
+    
+    console.log(this.un);
+    console.log(this.empty);
+    console.log(this.show);
   }
 }
